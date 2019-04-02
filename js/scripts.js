ea2900301eddb55d97f385d952bd0451d6769280
Liz Kelley and Marc Davies <marc@>
2019-04-02 15:57:01 -0700

    add initial file structure, add html structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
-$(function() {
-  $("form#luck-survey").submit(function(event){
-    event.preventDefault();
-    var luckyCount = [];
-    var unluckyCount = [];
-
-    $("input:checkbox[name=lucky]:checked").each(function() {
-      var luckyResponse = luckyCount.push($(this).val());
-    });
-    $("input:checkbox[name=unlucky]:checked").each(function() {
-      var unluckyResponse = unluckyCount.push($(this).val());
-    });
-
-    if(luckyCount.length > unluckyCount.length) {
-      $("#luckyOutput").show();
-    } else if(unluckyCount.length > luckyCount.length) {
-      $("#unluckyOutput").show();
-    } else if(unluckyCount.length === luckyCount.length) {
-      $("#neutralOutput").show();
-    }
-  });
-});
+// $(function() {
+//   $("form#luck-survey").submit(function(event){
+//     event.preventDefault();
+//     var luckyCount = [];
+//     var unluckyCount = [];
+//
+//     $("input:checkbox[name=lucky]:checked").each(function() {
+//       var luckyResponse = luckyCount.push($(this).val());
+//     });
+//     $("input:checkbox[name=unlucky]:checked").each(function() {
+//       var unluckyResponse = unluckyCount.push($(this).val());
+//     });
+//
+//     if(luckyCount.length > unluckyCount.length) {
+//       $("#luckyOutput").show();
+//     } else if(unluckyCount.length > luckyCount.length) {
+//       $("#unluckyOutput").show();
+//     } else if(unluckyCount.length === luckyCount.length) {
+//       $("#neutralOutput").show();
+//     }
+//   });
+// });
