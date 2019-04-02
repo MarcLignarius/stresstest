0a6745ec941dddedde8e4c14929351a27f3711c6
Liz Kelley and Marc Davies <liz@>
2019-04-02 16:46:28 -0700

    hide results in css, add results in html, do js logic that will return 3 responses based on user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
-// $(function() {
-//   $("form#luck-survey").submit(function(event){
-//     event.preventDefault();
-//     var luckyCount = [];
-//     var unluckyCount = [];
-//
-//     $("input:checkbox[name=lucky]:checked").each(function() {
-//       var luckyResponse = luckyCount.push($(this).val());
-//     });
-//     $("input:checkbox[name=unlucky]:checked").each(function() {
-//       var unluckyResponse = unluckyCount.push($(this).val());
-//     });
-//
-//     if(luckyCount.length > unluckyCount.length) {
-//       $("#luckyOutput").show();
-//     } else if(unluckyCount.length > luckyCount.length) {
-//       $("#unluckyOutput").show();
-//     } else if(unluckyCount.length === luckyCount.length) {
-//       $("#neutralOutput").show();
-//     }
-//   });
-// });
+$(function() {
+  $("form#health-survey").submit(function(event){
+    event.preventDefault();
+    var eventsCount = [];
+    var symptomsCount = [];
+    var relieverCount = [];
+
+    $("input:checkbox[name=stressful-events]:checked").each(function() {
+      var eventsResponse = eventsCount.push($(this).val());
+      console.log("eventsResponse", eventsResponse);
+    });
+    $("input:checkbox[name=symptoms]:checked").each(function() {
+      var symptomsResponse = symptomsCount.push($(this).val());
+        console.log("symptomsResponse", symptomsResponse);
+    });
+    $("input:checkbox[name=stress-relievers]:checked").each(function() {
+      var relieverResponse = relieverCount.push($(this).val());
+      console.log("relieverResponse", relieverResponse);
+    });
+    if((eventsCount.length + symptomsCount.length) > relieverCount.length)  {
+      $("#dobetter").show();
+    } else if((eventsCount.length + symptomsCount.length) === relieverCount.length) {
+      $("#ok").show();
+    } else if((eventsCount.length + symptomsCount.length) < relieverCount.length) {
+      $("#great").show();
+    }
+  });
+});
